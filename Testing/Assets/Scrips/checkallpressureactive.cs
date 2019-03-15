5d66de3368a9573138562de58ef0ea59e52162b9
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2019-03-15 16:13:27 +0100

    Added multitrigger
    Added a door that only opens when two pressureplates are both active

---
+++ Testing/Assets/Scrips/checkallpressureactive.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkallpressureactive : MonoBehaviour
+{
+    //Creating the game objects for panel 1, 2 and 3
+    public GameObject panel1;
+    public GameObject panel2;
+
+    //Creating the game object for activate door
+    public GameObject activateDoor;
+
+    //Creating triggers with the information it gets from the script inputtrigger.cs
+    private presureplatetrigger pressure1;
+    private presureplatetrigger pressure2;
+
+    //Creating a private doormoving with the information it gets from the script movedoor.cs
+    private movedoor doormoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the panels as triggers
+        pressure1 = panel1.GetComponent(typeof(presureplatetrigger)) as presureplatetrigger;
+        pressure2 = panel2.GetComponent(typeof(presureplatetrigger)) as presureplatetrigger;
+        //set the doormoving as activatedoor
+        doormoving = activateDoor.GetComponent(typeof(movedoor)) as movedoor;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+    }
+}
---
+++ Testing/Assets/Scrips/multipressurehandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class multipressurehandler : ScriptableObject
+{
+    private int activeCount = 0;
+
+    public int maxcount = 2;
+
+    public bool both()
+    {
+        if(activeCount >= maxcount)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void enter()
+    {
+        activeCount++;
+    }
+
+    public void exit()
+    {
+        activeCount--;
+    }
+
+    public void Reset()
+    {
+        activeCount = 0;
+    }
+}
